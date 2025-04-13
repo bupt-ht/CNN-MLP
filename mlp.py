54166aa2dd04f356a3dbf6490d2836d0098df4f4
bupt-ht <77781845+bupt-ht@users.noreply.github.com>
2025-04-13 10:46:51 +0800

    Add files via upload

---
+++ mlp.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class MLP(nn.Module):
+    def __init__(self):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(32*32*3, 1024)
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc3 = nn.Linear(512, 10)
+        self.dropout = nn.Dropout(0.2)
+
+    def forward(self, x):
+        x = x.view(-1, 32*32*3)  # 展平输入
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        return x
